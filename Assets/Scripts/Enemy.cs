2a2c18b7c5cb87784fc713c4670df8165229d71d
aktmdgus991 <s12249jiege@gmail.com>
2023-10-23 11:42:21 +0900

    Added Room/Enemy Generation

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy
+{
+    public string enemyName;
+    public int enemyTier;
+    public int enemyCost;
+    public Enemy(string _name, int _tier, int _cost)
+    {
+        enemyName = _name;
+        enemyTier = _tier;
+        enemyCost = _cost;
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Room : ScriptableObject
+{
+    public int bonusCost;
+    public int enemyCost;
+    public int wave;
+    public int enemyTier;
+    public Room(int _bonusCost, int _enemyCost, int _wave, int _enemyTier)
+    {
+        bonusCost = _bonusCost;
+        enemyCost = _enemyCost;
+        wave = _wave;
+        enemyTier = _enemyTier;
+    }
+}
