e41f40044288a99e54cb6d0be3b2df78328269d2
TokinoRitsu <s12249jiege@gmail.com>
2023-12-12 19:27:51 +0900

    minor changes

---
+++ Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomGenerator : MonoBehaviour
+{
+    public string roomShape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /*
+     * ' ' = nothing
+     * 'O' = floor
+     * 'P' = Player Spawn Point
+     * 'E' = Entrance
+     * 'X' = Exit
+     * Numbers = Enemy
+     * Shift+Numbers = Enemy with heal
+     * 
+     *   OOO OOOO
+     *  OOOO OOOO
+     *  OOOO OOOO
+     *  OOOO OOOO
+     * XOOOO OOOPE
+     *  OO@O O2OO
+     *  OOOO OOOO
+     *   OOO OOOO
+    */
+
+    public void GenerateRoom(string room)
+    {
+        foreach (char i in room)
+        {
+            switch (i)
+            {
+                case ' ':
+                    break;
+
+            }
+        }
+    }
+}
