e994d1333c4989c6a4cde91c59786368d0d52d8a
TokinoRitsu <s12249jiege@gmail.com>
2023-12-13 14:19:33 +0900

    Generation

---
+++ Assets/Scripts/Room/Room.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+public class Room
+{
+    public class SpawnPoint
+    {
+        public Vector3 pos;
+        public string mobName;
+        public bool hasHeal;
+        public SpawnPoint(int x, int z, string name, bool _hasHeal = false, int y = 0)
+        {
+            pos = new Vector3(x, y, z);
+            mobName = name;
+            hasHeal = _hasHeal;
+        }
+    }
+
+    public string roomShape;
+    public List<SpawnPoint> spawnPoints;
+}
---
+++ Assets/Scripts/Room/RoomGenerator.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class RoomGenerator : MonoBehaviour
+{
+    public Room room = new Room();
+    
+    public GameObject[] RoomTilePrefabs;
+
+    private Dictionary<char, string> CharToName = new Dictionary<char, string>
+    {
+        { 'O', "Floor1" },
+        { 'E', "Entrance" },
+        { 'X', "Exit" },
+        { 'a', "WallInnerL" },
+        { 'b', "WallInnerGamma" },
+        { 'c', "WallInnerReverseL" },
+        { 'd', "WallInnerReverseGamma" },
+        { 'e', "WallOuterL" },
+        { 'f', "WallOuterGamma" },
+        { 'g', "WallOuterReverseL" },
+        { 'h', "WallOuterReverseGamma" },
+        { 'i', "WallUp" },
+        { 'j', "WallDown" },
+        { 'k', "WallLeft" },
+        { 'l', "WallRight"},
+        { 'm', "WallUL"},
+        { 'n', "WallUR"},
+        { 'o', "WallDL"},
+        { 'p', "WallDR"},
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        room.roomShape = 
+@"
+ pjjjjjjj h
+pcOOO OOOO 
+lOOOO OOOOk
+lOOOO OOOOk
+lOOOO OOOOk
+XOOOO OOOOE
+lOOOO OOOOk
+lOOOO OOOOk
+lOOOO OOOOk
+ndOOO OOOO 
+ niiiiiii g";
+        GenerateRoom(room.roomShape);
+    }
+
+    /*
+     *  pjjjjjjj h
+        pcOOO OOOO 
+        lOOOO OOOOk
+        lOOOO OOOOk
+        lOOOO OOOOk
+        XOOOO OOOOE
+        lOOOO OOOOk
+        lOOOO OOOOk
+        lOOOO OOOOk
+        ndOOO OOOO 
+         niiiiiii g
+    */
+
+    public void GenerateRoom(string _roomShape)
+    {
+        Vector3 pos = new Vector3(0, 0, 0);
+        foreach (char i in _roomShape)
+        {
+            Debug.Log(i.ToString());
+            if (i == '\n')
+            {
+                pos = new Vector3(0, pos.y, pos.z - 1);
+            }
+            else
+            {
+                GenerateTile(i, pos);
+                pos = new Vector3(pos.x + 1, pos.y, pos.z);
+            }
+        }
+    }
+
+    public void GenerateTile(char tile, Vector3 pos)
+    {
+        if (tile != ' ' && tile != System.Environment.NewLine.ToCharArray()[0])
+        {
+            GameObject TileObject = Instantiate(GetGameObject(CharToName[tile]));
+            TileObject.transform.position = pos;
+        }
+    }
+
+    public GameObject GetGameObject(string _name)
+    {
+        GameObject result = null;
+        foreach (GameObject _object in RoomTilePrefabs)
+        {
+            if (_object.name == _name)
+            {
+                result = _object;
+                break;
+            }
+        }
+        return result;
+    }
+}
